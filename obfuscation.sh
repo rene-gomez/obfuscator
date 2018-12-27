4f0af1454ef38551cb0aacb2e8a6040fd85826fa
Rene Gómez <rene.gom3z@gmail.com>
2018-12-27 13:12:57 -0600

    Version 1.3 add new algorithm. 5000% more fast

--- obfuscation.sh
+++ obfuscation.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
+# version 1.3
 if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied, please supplied the directory"
-    exit 0
+        then
+        echo "No arguments supplied, please supplied the directory"
+        exit 0
 fi
+
 for file in $1/*
 do
-
-
         if [[ -s "$file" && ${file: -4} == ".log" ]]; then
-                echo -e "\n Start obfuscation process in file: $file"
-        .       cp $file $file.obfuscated
-                grep -oP '((?<=plainPassword:\s)|(?<=Authorization=B)|(?<=password=)|(?<=client_id=)|(?<=Bearer Exitosamente:\s)|(?<="access_token":")|(?<="password":"))\w+[A-Za-z0-9\-\_\s]+' $file.obfuscated|xargs -I '{}' sed -i 's/{}/*****/g' $file.obfuscated
-                echo -e "\n Done obfuscation in file: $file"
+                ds=`date '+%d/%m/%Y %H:%M:%S'`
+                echo -e "\n Start obfuscation process in file: $file at $ds"
+                perl -p -i -e 's/((?<=plainPassword:\s)|(?<=Authorization=B)|(?<=Authorization Bearer\s)|(?<=Authorization Basic\s)|(?<=encrypted Pass:\s)|(?<=password=)|(?<=client_id=)|(?<=Bearer Exitosamente:\s)|(?<="access_token":")|(?<="password":"))\w+[A-Za-z0-9\-\_\s]+/*****/g' $file
+                de=`date '+%d/%m/%Y %H:%M:%S'`
+                echo -e "\n Done obfuscation in file: $file at $de"
         fi
 done
+
+ 
